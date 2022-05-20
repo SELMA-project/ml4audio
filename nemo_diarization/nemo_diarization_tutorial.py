49624eb0812c0e3dacf93c8c915fca25efc9ca86
Tilo Himmelsbach <dertilo@gmail.com>
2022-05-20 19:36:15 +0200

    nemo_diarization_tutorial.py

---
+++ nemo_diarization/nemo_diarization_tutorial.py
@@ -0,0 +1,116 @@
+import json
+import os
+
+import matplotlib.pyplot
+import wget
+
+from nemo.collections.asr.models import ClusteringDiarizer
+from nemo.collections.asr.parts.utils.speaker_utils import (
+    rttm_to_labels,
+    labels_to_pyannote_object,
+)
+from pyannote.core import Annotation
+from omegaconf import OmegaConf
+
+# see: https://github.com/NVIDIA/NeMo/blob/main/tutorials/speaker_tasks/Speaker_Diarization_Inference.ipynb
+
+def get_test_data(data_dir):
+    an4_audio = os.path.join(data_dir, "an4_diarize_test.wav")
+    an4_rttm = os.path.join(data_dir, "an4_diarize_test.rttm")
+    if not os.path.exists(an4_audio):
+        an4_audio_url = (
+            "https://nemo-public.s3.us-east-2.amazonaws.com/an4_diarize_test.wav"
+        )
+        an4_audio = wget.download(an4_audio_url, data_dir)
+    if not os.path.exists(an4_rttm):
+        an4_rttm_url = (
+            "https://nemo-public.s3.us-east-2.amazonaws.com/an4_diarize_test.rttm"
+        )
+        an4_rttm = wget.download(an4_rttm_url, data_dir)
+    return an4_audio, an4_rttm
+
+
+def create_test_manifest(data_dir, an4_audio, an4_rttm):
+    meta = {
+        "audio_filepath": an4_audio,
+        "offset": 0,
+        "duration": None,
+        "label": "infer",
+        "text": "-",
+        "num_speakers": 2, # TODO: WTF!! what if I don't know how many speakers?
+        "rttm_filepath": an4_rttm,
+        "uem_filepath": None,
+    }
+    with open(f"{data_dir}/input_manifest.json", "w") as fp:
+        json.dump(meta, fp)
+        fp.write("\n")
+
+
+def get_test_config(data_dir):
+    MODEL_CONFIG = os.path.join(data_dir, "offline_diarization.yaml")
+    if not os.path.exists(MODEL_CONFIG):
+        config_url = "https://raw.githubusercontent.com/NVIDIA/NeMo/main/examples/speaker_tasks/diarization/conf/offline_diarization.yaml"
+        MODEL_CONFIG = wget.download(config_url, data_dir)
+
+    config = OmegaConf.load(MODEL_CONFIG)
+    print(OmegaConf.to_yaml(config))
+    return config
+
+
+if __name__ == "__main__":
+
+    ROOT = os.getcwd()
+    data_dir = os.path.join(ROOT, "data")
+    os.makedirs(data_dir, exist_ok=True)
+    an4_audio, an4_rttm = get_test_data(data_dir)
+
+    labels = rttm_to_labels(an4_rttm)
+    reference: Annotation = labels_to_pyannote_object(labels)
+    print(f"{labels=},{reference=}")
+    create_test_manifest(data_dir, an4_audio, an4_rttm)
+    config = get_test_config(data_dir)
+
+    output_dir = os.path.join(ROOT, "outputs")
+
+    pretrained_vad = "vad_marblenet"
+    pretrained_speaker_model = "titanet_large"
+
+    config.diarizer.manifest_filepath = "data/input_manifest.json"
+    config.diarizer.out_dir = (
+        output_dir  # Directory to store intermediate files and prediction outputs
+    )
+
+    config.diarizer.speaker_embeddings.model_path = pretrained_speaker_model
+    config.diarizer.speaker_embeddings.parameters.window_length_in_sec = 1.5
+    config.diarizer.speaker_embeddings.parameters.shift_length_in_sec = 0.75
+    # config.diarizer.speaker_embeddings.oracle_vad_manifest = None
+
+    config.diarizer.oracle_vad = (
+        False  # compute VAD provided with model_path to vad config
+    )
+    config.diarizer.clustering.parameters.oracle_num_speakers = True
+
+    # Here we use our inhouse pretrained NeMo VAD
+    config.diarizer.vad.model_path = pretrained_vad
+    config.diarizer.vad.window_length_in_sec = 0.15
+    config.diarizer.vad.shift_length_in_sec = 0.01
+    config.diarizer.vad.parameters.onset = 0.8
+    config.diarizer.vad.parameters.offset = 0.6
+    config.diarizer.vad.parameters.min_duration_on = 0.1
+    config.diarizer.vad.parameters.min_duration_off = 0.4
+
+    sd_model = ClusteringDiarizer(cfg=config)
+
+    sd_model.diarize()
+
+    from nemo.collections.asr.parts.utils.vad_utils import plot
+
+    plot(
+        an4_audio,
+        f"{output_dir}/vad_outputs/overlap_smoothing_output_median_0.875/an4_diarize_test.median",
+        an4_rttm,
+        per_args=config.diarizer.vad.parameters,  # threshold
+    )
+    matplotlib.pyplot.show()
+
+    print(f"postprocessing_params: {config.diarizer.vad.parameters}")
---
+++ nemo_diarization/readme.md
@@ -0,0 +1,63 @@
+* [Speaker_Diarization_Inference.ipynb](https://github.com/NVIDIA/NeMo/blob/main/tutorials/speaker_tasks/Speaker_Diarization_Inference.ipynb) 
+```shell
+python nemo_diarization_tutorial.py
+
+labels=['0.298981 2.770311 A', '3.163901 5.147011 B'],reference=<pyannote.core.annotation.Annotation object at 0x7fa5fb3e8ca0>
+name: ClusterDiarizer
+num_workers: 4
+sample_rate: 16000
+batch_size: 64
+diarizer:
+  manifest_filepath: ???
+  out_dir: ???
+  oracle_vad: false
+  collar: 0.25
+  ignore_overlap: true
+  vad:
+    model_path: null
+    external_vad_manifest: null
+    parameters:
+      window_length_in_sec: 0.15
+      shift_length_in_sec: 0.01
+      smoothing: median
+      overlap: 0.875
+      onset: 0.4
+      offset: 0.7
+      pad_onset: 0.05
+      pad_offset: -0.1
+      min_duration_on: 0.2
+      min_duration_off: 0.2
+      filter_speech_first: true
+  speaker_embeddings:
+    model_path: ???
+    parameters:
+      window_length_in_sec: 1.5
+      shift_length_in_sec: 0.75
+      multiscale_weights: null
+      save_embeddings: false
+  clustering:
+    parameters:
+      oracle_num_speakers: false
+      max_num_speakers: 20
+      enhanced_count_thres: 80
+      max_rp_threshold: 0.25
+      sparse_search_volume: 30
+
+[NeMo I 2022-03-03 11:54:08 clustering_diarizer:130] Loading pretrained vad_marblenet model from NGC
+
+...
+
+postprocessing_params: {'window_length_in_sec': 0.15, 'shift_length_in_sec': 0.01, 'smoothing': 'median', 'overlap': 0.875, 'onset': 0.8, 'offset': 0.6, 'pad_onset': 0.05, 'pad_offset': -0.1, 'min_duration_on': 0.1, 'min_duration_off': 0.4, 'filter_speech_first': True}
+100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 1/1 [00:00<00:00,  3.80it/s]
+100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 1/1 [00:00<00:00,  1.83it/s]
+[NeMo W 2022-03-03 11:54:11 speaker_utils:335] cuda=False, using CPU for Eigen decompostion. This might slow down the clustering process.
+100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 1/1 [00:00<00:00, 29.77it/s]
+
+```
+
+### outputs
+* some rttm file!! -> whats that? never heard of this format!
+```shell
+SPEAKER an4_diarize_test 1   0.330   2.460 <NA> <NA> speaker_1 <NA> <NA>
+SPEAKER an4_diarize_test 1   3.210   1.890 <NA> <NA> speaker_0 <NA> <NA>
+```
