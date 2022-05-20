72c22c0cb304b9625d1b1d5284753b6aaca6e2ae
Tilo Himmelsbach <dertilo@gmail.com>
2022-05-20 09:53:48 +0200

    audio_io.py, torchaudio_utils.py

---
+++ audio_utils/audio_io.py
@@ -0,0 +1,213 @@
+import io
+import json
+import os
+from dataclasses import dataclass
+from io import BytesIO
+from pathlib import Path
+from typing import Optional, Union, Iterator, Any
+
+import numpy as np
+from beartype import beartype
+
+from audio_utils.torchaudio_utils import load_resample_with_torch
+from misc_utils.beartypes import NumpyFloat1DArray, NumpyInt16Dim1, Numpy1DArray
+from misc_utils.processing_utils import exec_command
+from misc_utils.utils import get_val_from_nested_dict, NOT_EXISTING, just_try
+
+MAX_16_BIT_PCM: float = float(2 ** 15)  # 32768.0 for 16 bit, see "format"
+
+
+@dataclass
+class FileLikeAudioDatum:
+    id: str
+    audio_source: Any  # BytesIO, ExFileObject
+    format: str
+
+
+@beartype
+def load_audio_array_from_filelike(
+    filelike: FileLikeAudioDatum,
+    target_sample_rate: int,
+    sample_rate: Optional[int] = None,
+    offset: float = 0.0,
+    duration: Optional[float] = None,
+) -> NumpyFloat1DArray:
+    # TODO: WTF why is fisher faster with nemo, but kaldi which is also wav, faster with torchaudio??
+    if filelike.format == "wav" and not any(
+        (s in filelike.audio_source for s in ["Fisher"])
+    ):
+        array = load_resample_with_nemo(
+            audio_filepath=filelike.audio_source,
+            offset=offset,
+            duration=duration,
+            target_sample_rate=target_sample_rate,
+        )
+
+    elif filelike.format in ["flac"]:
+        # TODO: torchaudio cannot load flacs?
+        array = load_resample_with_nemo(
+            audio_filepath=BytesIO(filelike.audio_source.read()),
+            offset=offset,
+            duration=duration,
+            target_sample_rate=target_sample_rate,
+        )
+    else:
+        torch_tensor = load_resample_with_torch(
+            data_source=filelike.audio_source,
+            sample_rate=sample_rate,
+            target_sample_rate=target_sample_rate,
+            offset=offset if offset > 0.0 else None,
+            duration=duration,
+            format=filelike.format,
+        )
+        array = torch_tensor.numpy()
+
+    return array
+
+
+def ffprobe(vf):
+    cmd = f'ffprobe -v error -print_format json -show_entries stream "{vf}"'
+    o, e = exec_command(cmd)
+    return json.loads("".join([x.decode("utf8") for x in o]))
+
+
+def get_video_file_ffprobe_stream_infos(vf: str) -> list[dict]:
+    info_data = ffprobe(vf)["streams"]
+
+    def generate_infos():
+        for s in info_data:
+            kvs = (
+                ("-".join(p), get_val_from_nested_dict(s, p))
+                for p in [
+                    ["codec_name"],
+                    ["codec_type"],
+                    ["tags", "language"],
+                    ["tags", "title"],
+                ]
+            )
+            yield {k: v for k, v in kvs if v != NOT_EXISTING}
+
+    stream_infos = list(generate_infos())
+    return stream_infos
+
+
+def build_audio_cmd(af, k, vf):
+    cmd = f'ffmpeg -i "{vf}" -y -filter_complex "[0:a:{k}]channelsplit=channel_layout=stereo[left][right]" -map "[left]" -c:a libopus -ar 16000 -ac 1 {af}_left.opus.ogg -map "[right]" -c:a libopus -ar 16000 -ac 1 {af}_right.opus.ogg'
+    # cmd = f'ffmpeg -i "{vf}" -y -map 0:a:{k} -q:a 0 -ac 1 -ar 16000 "{af}.mp3"'
+    return cmd
+
+
+@beartype
+def extract_streams_from_video_file(
+    vf: str,
+    audio_file_target_folder: str,
+    stream_infos: Optional[list[dict]] = None,
+    build_cmd_fun=build_audio_cmd,
+    codec_type="audio",
+) -> list[str]:
+    if stream_infos is None:
+        stream_infos = get_video_file_ffprobe_stream_infos(vf)
+
+    audio_streams = [s for s in stream_infos if s["codec_type"] == codec_type]
+    audio_files = []
+    for k, info_d in enumerate(audio_streams):
+        af = f"{audio_file_target_folder}/{vf}_lang_{info_d.get('tags-language', '')}_title_{info_d.get('tags-title', '')}_{k}"
+        cmd = build_cmd_fun(af, k, vf)
+
+        if not os.path.isfile(af):
+            os.makedirs(Path(af).parent, exist_ok=True)
+            print(exec_command(cmd))
+
+        if not os.path.isfile(af):
+            print(f"{af=} failed!")
+        audio_files.append(af)
+
+    return audio_files
+
+
+@beartype
+def load_and_resample_16bit_PCM(
+    audio_filepath: str,
+    target_sample_rate: int,
+    offset=0.0,
+    duration: Optional[float] = None,
+) -> NumpyInt16Dim1:
+    a = load_resample_with_nemo(audio_filepath, target_sample_rate, offset, duration)
+    a = convert_to_16bit_array(a)
+    # a = np.expand_dims(a, axis=1)  # TODO: why did I every want this?
+    return a
+
+
+@beartype
+def convert_to_16bit_array(a: NumpyFloat1DArray) -> NumpyInt16Dim1:
+    a = a / np.max(np.abs(a)) * (MAX_16_BIT_PCM - 1)
+    a = a.astype(np.int16)
+    return a
+
+
+@beartype
+def load_resample_with_nemo(
+    audio_filepath: Union[str, BytesIO],
+    target_sample_rate: Optional[int] = 16000,
+    offset=0.0,
+    duration: Optional[float] = None,
+) -> NumpyFloat1DArray:
+    from nemo.collections.asr.parts.preprocessing import AudioSegment
+
+    # cause nemo wants 0 if no duration
+    duration = 0 if duration is None else duration
+    audio = AudioSegment.from_file(
+        audio_filepath,
+        target_sr=target_sample_rate,
+        offset=offset,
+        duration=duration,
+        trim=False,
+    )
+    s = audio.samples.shape
+    a = audio.samples.squeeze()
+    return a
+
+
+@beartype
+def break_array_into_chunks(
+    array: Numpy1DArray, chunk_size: int
+) -> Iterator[Numpy1DArray]:
+    """
+    non-overlapping chunks
+    """
+    buffer = array.copy()
+    while len(buffer) > 0:
+        out = buffer[:chunk_size]
+        buffer = buffer[chunk_size:]
+        yield out
+
+
+@beartype
+def read_audio_chunks_from_file(
+    audio_filepath: str,
+    target_sample_rate: int,
+    offset=0.0,
+    duration=None,
+    chunk_duration=0.05,
+) -> Iterator[NumpyInt16Dim1]:
+    array = load_and_resample_16bit_PCM(
+        audio_filepath, target_sample_rate, offset, duration
+    )
+    return break_array_into_chunks(array, int(target_sample_rate * chunk_duration))
+
+
+@beartype
+def normalize_audio_array(array: Numpy1DArray) -> NumpyFloat1DArray:
+    """
+    copypasted from nvidia/nemo: TranscodePerturbation
+    """
+    att_factor = 0.8  # to avoid saturation while writing to wav
+    array = array.astype(np.float)
+
+    max_level = np.max(np.abs(array))
+    if max_level > 0.8:
+        norm_factor = att_factor / max_level
+        norm_samples = norm_factor * array
+    else:
+        norm_samples = array
+    return norm_samples
---
+++ audio_utils/torchaudio_utils.py
@@ -0,0 +1,122 @@
+from io import BytesIO
+from tarfile import ExFileObject
+from typing import Any, Optional, Union
+
+import torch
+import torchaudio
+from beartype import beartype
+from torch import float32
+from torchaudio.transforms import Resample
+
+from misc_utils.beartypes import TorchTensor1D
+
+resamplers: dict[str, Resample] = {}
+
+
+@beartype
+def get_resampler(
+    sample_rate: int, target_sample_rate: int, dtype: Optional[torch.dtype]
+) -> Resample:
+    """
+    could one use a singleton here?
+    """
+    global resamplers
+    key = f"{sample_rate}-{target_sample_rate}-{dtype}"
+    if key not in resamplers:
+        resamplers[key] = Resample(sample_rate, target_sample_rate, dtype=dtype)
+    return resamplers[key]
+
+
+@beartype
+def torchaudio_resample(
+    signal: TorchTensor1D, sample_rate: int, target_sample_rate: int
+) -> TorchTensor1D:
+    if target_sample_rate != sample_rate:
+        resampler = get_resampler(
+            sample_rate, target_sample_rate=target_sample_rate, dtype=float32
+        )
+        signal = resampler(signal)
+    return signal
+
+
+@beartype
+def torchaudio_info(audio_file: str) -> tuple[int, int, float]:
+    info = torchaudio.info(audio_file)  # torchaudio==0.8.0
+    num_frames = info.num_frames
+    sample_rate = info.sample_rate
+    duration = num_frames / sample_rate
+    return num_frames, sample_rate, duration
+
+
+@beartype
+def _parse_offset_for_torchaudio_load(
+    offset: Optional[Union[int, float]], sample_rate: Optional[int] = None
+) -> int:
+    if offset is None:
+        frame_offset = 0
+    elif isinstance(offset, float):
+        assert sample_rate is not None
+        frame_offset = round(offset * sample_rate)
+    else:
+        frame_offset = offset
+    return frame_offset
+
+
+@beartype
+def _parse_duration_for_torchaudio_load(
+    duration: Optional[Union[int, float]], sample_rate: Optional[int] = None
+) -> int:
+    if duration is None:
+        num_frames = -1
+    elif isinstance(duration, float):
+        assert sample_rate is not None
+        num_frames = round(duration * sample_rate)
+    else:
+        num_frames = duration
+    return num_frames
+
+
+@beartype
+def torchaudio_load(
+    data_source: Union[str, BytesIO, ExFileObject],
+    offset: Optional[Union[int, float]] = None,
+    duration: Optional[Union[int, float]] = None,
+    format: Optional[str] = None,
+    sample_rate: Optional[int] = None,
+) -> tuple[TorchTensor1D, int]:
+    if isinstance(data_source, BytesIO):
+        assert format is not None, f"when reading from BytesIO a format must be given"
+    signal, sample_rate = torchaudio.load(
+        data_source,
+        format=format,
+        frame_offset=_parse_offset_for_torchaudio_load(offset, sample_rate),
+        num_frames=_parse_duration_for_torchaudio_load(duration, sample_rate),
+    )
+    signal = signal.squeeze()
+    return signal, sample_rate
+
+
+torchaudio.utils.sox_utils.set_buffer_size(
+    16000
+)  # necessary for long audio-headers (mp3)
+
+
+@beartype
+def load_resample_with_torch(
+    data_source: Any,  # ExFileObject
+    format: Optional[str] = None,
+    sample_rate: Optional[int] = None,
+    target_sample_rate: Optional[int] = 16000,
+    offset: Optional[Union[int, float]] = None,
+    duration: Optional[Union[int, float]] = None,
+) -> TorchTensor1D:
+    """
+    Providing num_frames and frame_offset arguments will slice the resulting Tensor object while decoding.
+    :param duration: see: .../torchaudio/backend/sox_io_backend.py
+    """
+    signal, sample_rate = torchaudio_load(
+        data_source, offset, duration, format, sample_rate
+    )
+    signal = torchaudio_resample(signal.squeeze(), sample_rate, target_sample_rate)
+
+    return signal
