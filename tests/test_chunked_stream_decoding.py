9387ac679c063a8456dd2ab113ead9be51b54c04
Tilo Himmelsbach <dertilo@gmail.com>
2022-05-29 20:42:28 +0200

    ChunkedPyctcDecoder

--- tests/test_chunked_stream_decoding.py
+++ tests/test_chunked_stream_decoding.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+from dataclasses import dataclass
+from typing import Any
 
 import kenlm
 import numpy as np
@@ -13,6 +15,7 @@ from conftest import (
     assert_transcript_cer,
 )
 from data_io.readwrite_files import read_lines
+from misc_utils.buildable import Buildable
 from misc_utils.prefix_suffix import PrefixSuffix
 from ml4audio.asr_inference.logits_cutter import LogitsCutter
 from ml4audio.audio_utils.overlap_array_chunker import MessageChunk
@@ -57,28 +60,73 @@ lm_data: KenLMForPyCTCDecodeFromArpa = KenLMForPyCTCDecodeFromArpa(
 unigrams = list(read_lines(lm_data.unigrams_filepath))
 
 
-@pytest.mark.parametrize(
-    "decoder",
-    [
-        (
-            StreamingBeamSearchDecoderCTC(
-                Alphabet.build_alphabet(
-                    list(load_hfwav2vec2_base_tokenizer().get_vocab().keys())
-                ),
-                language_model=LanguageModel(
-                    kenlm_model=kenlm.Model(lm_data.arpa_filepath),
-                    unigrams=unigrams,
-                    alpha=1.0,
-                    beta=0.5,
-                    # unk_score_offset=unk_score_offset,
-                    # score_boundary=lm_score_boundary,
-                ),
+@dataclass
+class ChunkedPyctcDecoder(Buildable):
+    def _build_self(self) -> Any:
+        self.lc = LogitsCutter()
+        self.lc.reset()
+
+        self.decoder = StreamingBeamSearchDecoderCTC(
+            Alphabet.build_alphabet(
+                list(load_hfwav2vec2_base_tokenizer().get_vocab().keys())
+            ),
+            language_model=LanguageModel(
+                kenlm_model=kenlm.Model(lm_data.arpa_filepath),
+                unigrams=unigrams,
+                alpha=1.0,
+                beta=0.5,
+                # unk_score_offset=unk_score_offset,
+                # score_boundary=lm_score_boundary,
+            ),
+        )
+
+        language_model = self.decoder.model_container[self.decoder._model_key]
+        # start with single beam to expand on
+        self.beams = [EMPTY_START_BEAM]
+        # bpe we can also have trailing word boundaries ▁⁇▁ so we may need to remember breaks
+        self.params = DecodeParams(
+            beam_width=DEFAULT_BEAM_WIDTH,
+            beam_prune_logp=DEFAULT_PRUNE_LOGP,
+            token_min_logp=DEFAULT_MIN_TOKEN_LOGP,
+            prune_history=DEFAULT_PRUNE_BEAMS,
+            hotword_scorer=HotwordScorer.build_scorer(
+                hotwords=None, weight=DEFAULT_HOTWORD_WEIGHT
+            ),
+            lm_start_state=None,
+            language_model=language_model,
+        )
+        return self
+
+    def decode(self, ch: MessageChunk) -> tuple[list[IncrBeam], list[IncrBeam]]:
+        s_e = (ch.frame_idx, ch.frame_idx + len(ch.array))
+        left_part, right_part = self.lc.calc_left_right(ch.array, s_e)
+
+        frame_idx = ch.frame_idx
+        if left_part is not None:
+            for logits_col in left_part:
+                self.beams = self.decoder.incr_decode_step(
+                    self.beams, frame_idx, logits_col, self.params
+                )
+                frame_idx += 1
+
+            print(f"{frame_idx=}, {self.beams[0][0]=}")
+
+        right_beams: list[Beam] = self.beams
+        for k, logits_col in enumerate(right_part):
+            right_beams = self.decoder.incr_decode_step(
+                right_beams, frame_idx + k, logits_col, self.params
             )
-        ),
-    ],
-)
+        print(f"non-final: {frame_idx+k=}, {right_beams[0][0]=}")
+
+        if ch.end_of_signal:
+            self.beams = right_beams
+
+        incr_beams = [IncrBeam(*beam) for beam in self.beams]
+        incr_right_beams = [IncrBeam(*beam) for beam in right_beams]
+        return incr_beams, incr_right_beams
+
+
 def test_chunked_streaming_beam_search_decoder(
-    decoder,
     librispeech_logtis_file,
     librispeech_ref,
 ):
@@ -87,68 +135,12 @@ def test_chunked_streaming_beam_search_decoder(
     logits_chunks: list[MessageChunk] = list(
         overlapping_messages_from_array(logits, step_size=100, chunk_size=200)
     )
-    chunk_spans = [
-        (ch.array, (ch.frame_idx, ch.frame_idx + len(ch.array))) for ch in logits_chunks
-    ]
-
-    lc = LogitsCutter()
-    lc.reset()
-
-    left_right = [lc.calc_left_right(l, s_e) for l, s_e in chunk_spans]
-    print(f"{[(l.shape if l is not None else 0,r.shape) for l,r in left_right ]=}")
-    # parts = [l for l, r in left_right] + [left_right[-1][1]]
-    # parts = [x for x in parts if x is not None]
 
     max_cer = 0.007
+    decoder: ChunkedPyctcDecoder = ChunkedPyctcDecoder().build()
 
-    language_model = decoder.model_container[decoder._model_key]
-    # start with single beam to expand on
-    beams = [EMPTY_START_BEAM]
-    # bpe we can also have trailing word boundaries ▁⁇▁ so we may need to remember breaks
-    params = DecodeParams(
-        beam_width=DEFAULT_BEAM_WIDTH,
-        beam_prune_logp=DEFAULT_PRUNE_LOGP,
-        token_min_logp=DEFAULT_MIN_TOKEN_LOGP,
-        prune_history=DEFAULT_PRUNE_BEAMS,
-        hotword_scorer=HotwordScorer.build_scorer(
-            hotwords=None, weight=DEFAULT_HOTWORD_WEIGHT
-        ),
-        lm_start_state=None,
-        language_model=language_model,
-    )
-
-    frame_idx = 0
-    for left_part, right_part in left_right:
-
-        right_beams: list[Beam] = beams
-        for k, logits_col in enumerate(right_part):
-            right_beams = decoder.incr_decode_step(
-                right_beams, frame_idx + k, logits_col, params
-            )
-        incr_beams = [IncrBeam(*beam) for beam in right_beams]
-        print(f"non-final: {frame_idx+k=}, {incr_beams[0].text=}")
-
-        if left_part is not None:
-            for logits_col in left_part:
-                beams = decoder.incr_decode_step(beams, frame_idx, logits_col, params)
-                frame_idx += 1
-
-            incr_beams = [IncrBeam(*beam) for beam in beams]
-            print(f"{frame_idx=}, {incr_beams[0].text=}")
-
-    for logits_col in right_part:
-        beams = decoder.incr_decode_step(beams, frame_idx, logits_col, params)
-        frame_idx += 1
-
-    incr_beams = [IncrBeam(*beam) for beam in beams]
-    print(f"{frame_idx=}, {incr_beams[0].text=}")
-
+    for ch in logits_chunks:
+        final_beams, nonfinal_beams = decoder.decode(ch)
     ref = librispeech_ref
-    hyp = incr_beams[0].text
-    assert_transcript_cer(hyp, ref, max_cer)
-
-    out_beams = decoder.final_scoring_and_sorting(beams, params)
-
-    ref = librispeech_ref
-    hyp = out_beams[0].text
+    hyp = final_beams[0].text
     assert_transcript_cer(hyp, ref, max_cer)
