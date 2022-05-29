854c880fd621301b174e9cab316c8ad96b0096e2
Tilo Himmelsbach <dertilo@gmail.com>
2022-05-29 20:50:37 +0200

    refactor ChunkedPyctcDecoder

--- tests/test_chunked_stream_decoding.py
+++ tests/test_chunked_stream_decoding.py
@@ -1,7 +1,7 @@
 import os
 import shutil
-from dataclasses import dataclass
-from typing import Any
+from dataclasses import dataclass, field
+from typing import Any, Union, Optional
 
 import kenlm
 import numpy as np
@@ -16,12 +16,14 @@ from conftest import (
 )
 from data_io.readwrite_files import read_lines
 from misc_utils.buildable import Buildable
+from misc_utils.dataclass_utils import _UNDEFINED, UNDEFINED
 from misc_utils.prefix_suffix import PrefixSuffix
 from ml4audio.asr_inference.logits_cutter import LogitsCutter
 from ml4audio.audio_utils.overlap_array_chunker import MessageChunk
 from ml4audio.text_processing.asr_text_normalization import TranscriptNormalizer, Casing
 from ml4audio.text_processing.lm_model_for_pyctcdecode import (
     KenLMForPyCTCDecodeFromArpa,
+    KenLMForPyCTCDecode,
 )
 from ml4audio.text_processing.streaming_beam_search_decoder import (
     StreamingBeamSearchDecoderCTC,
@@ -51,36 +53,47 @@ tn = TranscriptNormalizer(
 )
 
 
-lm_data: KenLMForPyCTCDecodeFromArpa = KenLMForPyCTCDecodeFromArpa(
-    name="test",
-    cache_base=cache_base,
-    arpa_file=f"{TEST_RESOURCES}/lm.arpa",
-    transcript_normalizer=tn,
-).build()
-unigrams = list(read_lines(lm_data.unigrams_filepath))
-
-
 @dataclass
 class ChunkedPyctcDecoder(Buildable):
+
+    lm_weight: Union[_UNDEFINED, float] = UNDEFINED
+    beta: Union[_UNDEFINED, float] = UNDEFINED
+    # cannot do this with beartype NeList[str] for vocab, cause it might be a CachedList
+    # vocab: Union[_UNDEFINED, list[str]] = UNDEFINED
+
+    lm_data: Union[
+        KenLMForPyCTCDecode, _UNDEFINED
+    ] = UNDEFINED  # TODO: rename lm_data to lm_model
+
+    num_best: int = 1  # number of beams to return
+    beam_size: int = 100
+
+    _pyctc_decoder: Optional[StreamingBeamSearchDecoderCTC] = field(
+        init=False, repr=False, default=None
+    )
+
     def _build_self(self) -> Any:
         self.lc = LogitsCutter()
         self.lc.reset()
+        unigrams = list(read_lines(self.lm_data.unigrams_filepath))
 
-        self.decoder = StreamingBeamSearchDecoderCTC(
+        self._pyctc_decoder = StreamingBeamSearchDecoderCTC(
             Alphabet.build_alphabet(
                 list(load_hfwav2vec2_base_tokenizer().get_vocab().keys())
             ),
             language_model=LanguageModel(
-                kenlm_model=kenlm.Model(lm_data.arpa_filepath),
+                kenlm_model=kenlm.Model(self.lm_data.arpa_filepath),
                 unigrams=unigrams,
-                alpha=1.0,
-                beta=0.5,
+                alpha=self.lm_weight,
+                beta=self.beta,
                 # unk_score_offset=unk_score_offset,
                 # score_boundary=lm_score_boundary,
             ),
         )
 
-        language_model = self.decoder.model_container[self.decoder._model_key]
+        language_model = self._pyctc_decoder.model_container[
+            self._pyctc_decoder._model_key
+        ]
         # start with single beam to expand on
         self.beams = [EMPTY_START_BEAM]
         # bpe we can also have trailing word boundaries ▁⁇▁ so we may need to remember breaks
@@ -104,19 +117,18 @@ class ChunkedPyctcDecoder(Buildable):
         frame_idx = ch.frame_idx
         if left_part is not None:
             for logits_col in left_part:
-                self.beams = self.decoder.incr_decode_step(
+                self.beams = self._pyctc_decoder.incr_decode_step(
                     self.beams, frame_idx, logits_col, self.params
                 )
                 frame_idx += 1
 
-            print(f"{frame_idx=}, {self.beams[0][0]=}")
+            assert frame_idx == ch.frame_idx + len(left_part)
 
         right_beams: list[Beam] = self.beams
         for k, logits_col in enumerate(right_part):
-            right_beams = self.decoder.incr_decode_step(
+            right_beams = self._pyctc_decoder.incr_decode_step(
                 right_beams, frame_idx + k, logits_col, self.params
             )
-        print(f"non-final: {frame_idx+k=}, {right_beams[0][0]=}")
 
         if ch.end_of_signal:
             self.beams = right_beams
@@ -137,10 +149,22 @@ def test_chunked_streaming_beam_search_decoder(
     )
 
     max_cer = 0.007
-    decoder: ChunkedPyctcDecoder = ChunkedPyctcDecoder().build()
+    decoder: ChunkedPyctcDecoder = ChunkedPyctcDecoder(
+        lm_weight=1.0,
+        beta=0.5,
+        beam_size=100,
+        lm_data=KenLMForPyCTCDecodeFromArpa(
+            name="test",
+            cache_base=cache_base,
+            arpa_file=f"{TEST_RESOURCES}/lm.arpa",
+            transcript_normalizer=tn,
+        ),
+    ).build()
 
     for ch in logits_chunks:
         final_beams, nonfinal_beams = decoder.decode(ch)
+        print(f"{final_beams[0].text=}")
+        print(f"{nonfinal_beams[0].text=}")
     ref = librispeech_ref
     hyp = final_beams[0].text
     assert_transcript_cer(hyp, ref, max_cer)
