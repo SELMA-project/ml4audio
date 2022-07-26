5cf796f208031faf622bf9b33febc30f7e91309b
Tilo Himmelsbach <dertilo@gmail.com>
2022-07-26 21:48:19 +0200

    common_voice_datasets.py

--- ml4audio/audio_data/common_voice_datasets.py
+++ ml4audio/audio_data/common_voice_datasets.py
@@ -1,7 +1,9 @@
 import itertools
 import os
+import shutil
 from dataclasses import dataclass, field
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import Iterator, ClassVar, Any, Annotated, Optional
 
 from beartype.vale import Is
@@ -87,22 +89,33 @@ class CommonVoiceExtracted(Buildable):
         return f"{self.data_dir}/clips"
 
     def _download_and_extract_raw_data(self):
-        extract_folder = f"{self.data_base}/{self.name}"
-        os.makedirs(extract_folder, exist_ok=True)
+        """
+        # might be necessary to extract tar.gz here, somehow this raid that I am using does deadlocks/hangs forever when trying to extract big files into it
+        """
+        with TemporaryDirectory(prefix="common_voice_extract") as tmp_dir:
+            extract_folder = tmp_dir
+            print(f"{extract_folder=}")
+            os.makedirs(extract_folder, exist_ok=True)
 
-        if self.targz_file is None:
-            wget_file(f'"{self.url}"', extract_folder, verbose=True)
-            file = next(Path(extract_folder).glob(f"{self.name}*"))
-        else:
-            file = self.targz_file
+            if self.targz_file is None:
+                wget_file(f'"{self.url}"', extract_folder, verbose=True)
+                file = next(Path(extract_folder).glob(f"{self.name}*"))
+            else:
+                file = self.targz_file
 
-        print(f"extracting: {self.name}")
-        extract_file(
-            f'"{file}"', extract_folder, lambda dirr, file: f"tar xzf {file} -C {dirr}"
-        )
+            print(f"extracting: {self.name}")
+            extract_file(
+                f'"{file}"',
+                extract_folder,
+                lambda dirr, file: f"tar xzf {file} -C {dirr}",
+            )
 
-        if self.targz_file is None:
-            os.remove(file)
+            shutil.copytree(
+                extract_folder, f"{self.data_base}/{self.name}", dirs_exist_ok=True
+            )
+
+            if self.targz_file is None:
+                os.remove(file)
 
     @property
     def _is_ready(self) -> bool:
@@ -129,13 +142,14 @@ if __name__ == "__main__":
     BASE_PATHES["cache_root"] = cache_root
     BASE_PATHES["raw_data"] = PrefixSuffix("cache_root", "RAW_DATA")
 
-    url = "https://mozilla-common-voice-datasets.s3.dualstack.us-west-2.amazonaws.com/cv-corpus-10.0-2022-07-04/cv-corpus-10.0-2022-07-04-de.tar.gz?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAQ3GQRTO3NUOIJFM4%2F20220723%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220723T092446Z&X-Amz-Expires=43200&X-Amz-Security-Token=FwoGZXIvYXdzEDoaDHWugLCAcPgojreCESKSBAvSpfh%2BvzRbnLXAeqnyOr4iXCvpwutOFIAfhz%2FqipevlQwjR2A1mYjIJugei7A5NprV8jlhNbDUJegGjh%2B7TMVldauTyysyVAL8eY1QZoN0KEDVVPEbQwV2mdLF7K63dyyt15G5daqu8W3KgjL9KMftUpHPj2DK39vLBotYDzoGT3AHHMSmdhaW%2B5oqfdQZeoMDhwKhnFZpHgBT6VaEWf3nRL5ipz7aGZKHURRR6%2B%2FPFV2ajB1DXKFDSBdav9RgHUYbnMbCor%2FVoDurfoIS7CgyyMQxtwJZJv18iHK4ZZCbiAaYdq1L07Gr0ytzmAqu8Vc3qlyees2ou1b6a3AtU%2BJpyX%2Bj2borKdw0vPOC%2Bk%2BPkUvazFkP%2BEj%2FvlMokh%2FDqvh6w7by0UQt5WbAWIh%2Bl7bgEG0xQ1Gj4hXTeFkPPpkhw8GC4%2B7XhekyWBjIpf%2FyC9IEGLBSoUFY8f5g3mbYfcxlAXYXqW3tpYqxXSDsEetIyl%2BMG5i6XFKEZX24y%2FbTJmvbYDHf%2BpcCw4FGSkDP1xCF25OvXb6WWT7qCXo3%2FOTBYetukE19oU2UDSo4pQc3oBj%2BwROP8vT15JnDNtnSccfi7k45Nw11clN5J2EuliurJbnQAErWsdrM98k%2BGapjkGFWecve5FZRaB8ka8AZDJiHWg0RNiIl5WH9ux5qqZMiynNSxDtAKNqDLyLCQx4yi6SwKPnw7pYGMir2WbNNrKZG0oo3cZz50kELWsu7Iy8E%2BQteofOwj6wtuoxL9D2ftXGudn0%3D&X-Amz-Signature=e25ce4852a38d15d1172b0f016f72e63825d5396796500bd4a1d3a064246a019&X-Amz-SignedHeaders=host"
-    corpus: CommonVoiceExtracted = CommonVoiceExtracted(
-        targz_file=f"{BASE_PATHES['base_path']}/data/ASR_DATA/COMMON_VOICE/cv-corpus-10.0-2022-07-04-es.tar.gz",
-        url=None,
-    )
-    assert os.path.isdir(corpus.data_dir), corpus.data_dir
-    corpus.build()
+    urls = [
+    ]
+    for url in urls:
+        corpus: CommonVoiceExtracted = CommonVoiceExtracted(
+            targz_file=f"{BASE_PATHES['base_path']}/data/ASR_DATA/COMMON_VOICE/cv-corpus-10.0-2022-07-04-es.tar.gz",
+            # url=url,
+        )
+        corpus.build()
 
-    for o in itertools.islice(corpus.get_split_data("train"), 10):
-        print(o)
+        for o in itertools.islice(corpus.get_split_data("train"), 10):
+            print(o)
