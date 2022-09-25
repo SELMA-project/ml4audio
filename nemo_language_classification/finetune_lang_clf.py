b3434b91c22b93a2e97c2a26f4d2838c5e0edb72
Tilo Himmelsbach <dertilo@gmail.com>
2022-09-25 14:12:07 +0200

    finetune titanet

--- nemo_language_classification/finetune_lang_clf.py
+++ nemo_language_classification/finetune_lang_clf.py
@@ -1,4 +1,5 @@
 # based on: https://github.com/NVIDIA/NeMo/blob/aff169747378bcbcec3fc224748242b36205413f/examples/speaker_tasks/recognition/speaker_reco_finetune.py
+import os
 
 import pytorch_lightning as pl
 from omegaconf import OmegaConf
@@ -8,25 +9,37 @@ from nemo.collections.asr.models import EncDecSpeakerLabelModel
 from nemo.core.config import hydra_runner
 from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
+from nemo_language_classification.prepare_lang_clf_splits import create_subset_manifest
 
 seed_everything(42)
 
 
 @hydra_runner(config_path="conf", config_name="titanet-finetune.yaml")
 def main(cfg):
+    labels = ["en", "de", "es", "ru", "pt", "fr", "it"]
+    cfg.model.train_ds.manifest_filepath = f"{os.environ['BASE_PATH']}/data/AUDIO_DATA/lang_clf_data_7lanuages/train_manifest.jsonl"
+    cfg.model.validation_ds.manifest_filepath = f"{os.environ['BASE_PATH']}/data/AUDIO_DATA/lang_clf_data_7lanuages/validation_manifest.jsonl"
+    # cfg.model.test_ds.manifest_filepath = f"{os.environ['BASE_PATH']}/data/AUDIO_DATA/lang_clf_data_7lanuages/test_manifest.jsonl"
+    cfg.model.decoder.num_classes = len(labels)
 
-    logging.info(f'Hydra config: {OmegaConf.to_yaml(cfg)}')
+    logging.info(f"Hydra config: {OmegaConf.to_yaml(cfg)}")
     trainer = pl.Trainer(**cfg.trainer)
-    _ = exp_manager(trainer, cfg.get("exp_manager", None))
-    speaker_model = EncDecSpeakerLabelModel(cfg=cfg.model, trainer=trainer)
-    speaker_model.maybe_init_from_pretrained_checkpoint(cfg)
-    trainer.fit(speaker_model)
+    exp_man_cfg = cfg.get("exp_manager", None)
+    _ = exp_manager(trainer, exp_man_cfg)
+    mdl = EncDecSpeakerLabelModel(cfg=cfg.model, trainer=trainer)
+    mdl.maybe_init_from_pretrained_checkpoint(cfg)
+    mdl.cfg.train_ds.labels = labels  # TODO(tilo):WTF! had to manually stick labels in there, so that I have "vocabulary" at inference-time
 
-    if hasattr(cfg.model, 'test_ds') and cfg.model.test_ds.manifest_filepath is not None:
-        trainer = pl.Trainer(devices=1, accelerator=cfg.trainer.accelerator)
-        if speaker_model.prepare_test(trainer):
-            trainer.test(speaker_model)
+    trainer.fit(mdl)
+
+    # if (
+    #     hasattr(cfg.model, "test_ds")
+    #     and cfg.model.test_ds.manifest_filepath is not None
+    # ):
+    #     trainer = pl.Trainer(devices=1, accelerator=cfg.trainer.accelerator)
+    #     if mdl.prepare_test(trainer):
+    #         trainer.test(mdl)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
