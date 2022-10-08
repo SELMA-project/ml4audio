9852c6200dd606b853959be8544080b179cc3c98
Tilo Himmelsbach <dertilo@gmail.com>
2022-10-08 09:13:18 +0200

    test_fastapi_asr_service.py

--- fastapi_asr_service/tests/test_fastapi_asr_service.py
+++ fastapi_asr_service/tests/test_fastapi_asr_service.py
@@ -1,5 +1,6 @@
 import os.path
 
+import icdiff
 import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
@@ -31,17 +32,29 @@ def test_transcripe_endpoint(test_client, audio_file, transcript_reference):
     assert os.path.isdir(
         os.environ.get("CACHE_ROOT", "no-dir")
     ), "CACHE_ROOT where Aschinglupi got exported must be set as env-variable!"
-    max_CER = 0.0
+    max_CER = 0.02
+
+    f = open(audio_file, "rb")
+    files = {"file": (f.name, f, "multipart/form-data")}
 
     resp = test_client.post(
         "/transcribe",
-        json={
-            "file": audio_file,
-        },
+        files=files,
     )
 
     assert resp.status_code == status.HTTP_200_OK
-    hyp = resp.text
+    hyp = resp.json()["text"]
+    ref = transcript_reference.upper()
+    # cd = icdiff.ConsoleDiff(cols=120)
+    # diff_line = "\n".join(
+    #     cd.make_table(
+    #         [ref],
+    #         [hyp],
+    #         "ref",
+    #         "hyp",
+    #     )
+    # )
+    # print(diff_line)
 
     cer = calc_cer([(hyp, transcript_reference)])
     assert cer <= max_CER
