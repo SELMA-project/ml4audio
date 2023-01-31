de6850a8cc0a00e84641bcb075660d28899b32b6
Tilo Himmelsbach <dertilo@gmail.com>
2023-01-31 13:16:36 +0100

    LentaData

--- nemo_punctuation_capitalization/punctcap_training/lenta_data.py
+++ nemo_punctuation_capitalization/punctcap_training/lenta_data.py
@@ -1,24 +1,63 @@
 import itertools
 import os.path
-import re
-import unicodedata
 from dataclasses import dataclass, field
 from typing import Iterable, Iterator, Any
 
 import pandas
-import regex
-from misc_utils.buildable_data import BuildableData
-from pandas import Series
-from tqdm import tqdm
-
 from data_io.download_extract_files import wget_file
-from data_io.readwrite_files import write_lines
-from misc_utils.buildable import Buildable
+from misc_utils.buildable_data import BuildableData
 from misc_utils.prefix_suffix import PrefixSuffix, BASE_PATHES
+from pandas import Series
+
+"""
+this code is completely unused!
+def clean_russian_text_for_punctcap_training(text: str, punct_marks: str = ",?."):
+    copypasted from tugtekins "russian_normalization_train"-method <fhg-gitlab>/mturan/russian-punct-casing/-/blob/master/local/get_lenta_data.py#L13
+    # TODO(tilo): @tugtekin -> please explanation for: NFKC, re.sub, regex.sub, !
+        we need pytests!
+        written2spoken before this cleaning method, OR adapt cleaning method as to keep numbers!
+    Normalization and deRomanization of Russian data for training purpose
+    Args:
+        text: input text
+        punct_marks: supported punctuation marks
+    # \xa0 (i.e. chr(160)) creates problems in Lenta dataset
+    # this is a non-breaking space in Latin1 (ISO 8859-1)
+    # for other languages (with Latin alphabet) better to use 'NFKD' instead of 'NFKC'
+    unicoded = unicodedata.normalize("NFKC", text)
+
+    # remove links if exist
+    no_URL = re.sub(r"http\S+", "", unicoded)
+
+    # delete alphanumeric Latin letters except for Cyrillic (no transliteration)
+    # also, remove all the punctuations except defined in 'punct_marks'
+    match = "[^\s\p{IsCyrillic}" + punct_marks + "]"
+    only_cyrillic_and_punctuations = regex.sub(match, "", no_URL)
+
+    # remove repetitive whitespace
+    normalized = " ".join(only_cyrillic_and_punctuations.split())
+
+    # replace punctuations with extra spaces
+    # e.g. "hey . you are , okay ?" --> "hey. you are, okay?"
+    remove_extra_space = {" ,": ",", " .": ".", " ?": "?"}
+    for key, value in remove_extra_space.items():
+        normalized = normalized.replace(key, value)
+
+    return normalized
+"""
+
+
+def got_text(d) -> bool:
+    return (
+        isinstance(d, tuple) and isinstance(d[1], Series) and isinstance(d[1].text, str)
+    )
 
 
 @dataclass
 class LentaData(BuildableData, Iterable[str]):
+
+    """
+    is simply downloading the lenta-ru-news.csv.bz2 file into the "russian_text_data"-folder
+    """
 
     punct_marks: str = ",?."  # TODO: !
 
@@ -61,25 +100,16 @@ class LentaData(BuildableData, Iterable[str]):
                 original = d[1].text.replace("\n", " ").replace("\r", "")
                 yield original
 
-    def got_text(d) -> bool:
-        return (
-            isinstance(d, tuple)
-            and isinstance(d[1], Series)
-            and isinstance(d[1].text, str)
-        )
-
 
 if __name__ == "__main__":
 
-    base_path = os.environ["BASE_PATH"]
-    processed_corproa_dir = "/nm-raid/audio/data/corpora/processed_corpora"
-    # cache_root = f"{base_path}/data/cache"
+    processed_corproa_dir = f"{os.environ['corpora']}/processed_corpora"
     BASE_PATHES["processed_corproa_dir"] = processed_corproa_dir
-    # BASE_PATHES["processed_data"] = PrefixSuffix("cache_root", "PROCESSED_DATA")
+    BASE_PATHES["russian_text_data"] = PrefixSuffix(
+        "processed_corproa_dir", "RUSSIAN_TEXT_DATA"
+    )
 
     corpus = LentaData().build()
-    list(tqdm(corpus))
+
     for t in itertools.islice(corpus, 0, 10):
         print(f"{t=}")
-    # wget -q ${lenta} -P ${resource_fold} || exit 1
-    # bzip2 -d ${resource_fold}/${file_base} || exit 1
