b30684985067a9bde6578eb535395691a7bfcacd
Tilo Himmelsbach <dertilo@gmail.com>
2023-03-08 09:21:37 +0100

    more generic whisper inferencer

--- ml4audio/asr_inference/whisper_inferencer.py
+++ ml4audio/asr_inference/whisper_inferencer.py
@@ -1,25 +1,24 @@
 import os
-from dataclasses import dataclass, field
-from typing import Iterator, Any, Union, Optional
+from dataclasses import dataclass, field, asdict
+from typing import Iterator, Any, Union, Optional, Annotated
 
 from beartype import beartype
+from beartype.vale import Is
 
 import whisper as whisper_module
 from misc_utils.beartypes import NumpyFloat1DArray
 from misc_utils.buildable_data import BuildableData
 from misc_utils.prefix_suffix import PrefixSuffix, BASE_PATHES
-from ml4audio.audio_utils.audio_data_models import IdText
 from whisper import Whisper
+
+WHISPER_TASKS = {"transcribe", "translate"}
 
 
 @dataclass
-class WhisperInferencer(BuildableData):
-    """
-    https://github.com/saharmor/whisper-playground
-    """
-    model_name: str = "base"
-    lang: str = "de"
-
+class WhisperPredictArgs:
+    audio: NumpyFloat1DArray
+    task: Annotated[str, Is[lambda s: s in WHISPER_TASKS]]
+    language: str = "de"
     temperature: Optional[Union[float, tuple[float, ...], list[float]]] = (
         0.0,
         0.2,
@@ -28,7 +27,18 @@ class WhisperInferencer(BuildableData):
         0.8,
         1.0,
     )  # this is default in whisper code
-    beam_size: Optional[int] = None # default=5 see whisper code
+    # don't mess with the temperatures! they are needed for fallback if beam-search fails!
+    beam_size: Optional[int] = None  # default=5 see whisper code
+
+
+@dataclass
+class WhisperInferencer(BuildableData):
+    """
+    https://github.com/saharmor/whisper-playground
+    """
+
+    model_name: str = "base"
+
     _model: Whisper = field(init=False, repr=False)
     base_dir: PrefixSuffix = field(
         default_factory=lambda: PrefixSuffix("cache_root", "MODELS/WHISPER_MODELS")
@@ -64,22 +74,22 @@ class WhisperInferencer(BuildableData):
         self._model = whisper_module.load_model(self._checkpoint_file)
 
     @beartype
-    def predict(self, audio_array: NumpyFloat1DArray) -> str:
+    def predict(self, pred_args: WhisperPredictArgs) -> dict:
 
-        result = self._model.transcribe(
-            audio_array,
-            task="transcribe",
-            language=self.lang,
-            beam_size=self.beam_size,
-            temperature=self.temperature,  # don't mess with the temperatures! they are needed for fallback if beam-search fails!
-        )
-        return result["text"].strip(" ")
+        result = self._model.transcribe(**asdict(pred_args))
+        # TODO: result as dataclass?
+        return result
 
 
 if __name__ == "__main__":
-    base_path = os.environ["BASE_PATH"]
+    base_path = os.environ.get("BASE_PATH", "/tmp")
     cache_root = f"{base_path}/data/cache"
     BASE_PATHES["cache_root"] = cache_root
 
-    whisper = WhisperInferencer(model_name="tiny", lang="de")
-    whisper.build()
+    inferencer = WhisperInferencer(model_name="tiny")
+    inferencer.build()
+    from ml4audio.audio_utils.audio_io import ffmpeg_load_trim
+
+    file = "tests/resources/LibriSpeech_dev-other_116_288046_116-288046-0011.opus"
+    array = ffmpeg_load_trim(file)
+    print(f"{inferencer.predict(array)=}")
