d6798ded4357d13125bc5ad488c9602e229576de
Tilo Himmelsbach <dertilo@gmail.com>
2023-08-23 08:06:15 +0200

    FasterWhisperArgs

--- ml4audio/asr_inference/faster_whisper_inferencer.py
+++ ml4audio/asr_inference/faster_whisper_inferencer.py
@@ -1,9 +1,10 @@
-from dataclasses import dataclass, field
-from typing import Any, Optional
+from dataclasses import dataclass, field, asdict
+from typing import Any, Optional, Union, Iterable
 
 import faster_whisper
 from beartype import beartype
 from faster_whisper import download_model, WhisperModel
+from faster_whisper.vad import VadOptions
 
 import whisper as whisper_module
 from misc_utils.beartypes import NumpyFloat1D
@@ -12,6 +13,45 @@ from ml4audio.asr_inference.whisper_inference import (
     WhisperInferencer,
     WhisperArgs,
 )
+
+
+@dataclass(frozen=True)
+class FasterWhisperArgs(WhisperArgs):
+    """
+    this is based on the faster-whispers transcribe-methods input arguments
+    see: https://github.com/guillaumekln/faster-whisper/blob/7b271da0351e4f81f80e8bb4d2c21c9406475aa9/faster_whisper/transcribe.py#L155
+    """
+
+    # audio: Union[str, BinaryIO, np.ndarray],
+    language: Optional[str] = None
+    task: str = "transcribe"
+    beam_size: int = 5
+    best_of: int = 5
+    patience: float = 1
+    length_penalty: float = 1
+    temperature: Union[float, list[float], tuple[float, ...]] = (
+        0.0,
+        0.2,
+        0.4,
+        0.6,
+        0.8,
+        1.0,
+    )
+    compression_ratio_threshold: Optional[float] = 2.4
+    log_prob_threshold: Optional[float] = -1.0
+    no_speech_threshold: Optional[float] = 0.6
+    condition_on_previous_text: bool = True
+    initial_prompt: Optional[Union[str, Iterable[int]]] = None
+    prefix: Optional[str] = None
+    suppress_blank: bool = True
+    suppress_tokens: Optional[list[int]] = field(default_factory=lambda: [-1])
+    without_timestamps: bool = False
+    max_initial_timestamp: float = 1.0
+    word_timestamps: bool = False
+    prepend_punctuations: str = "\"'“¿([{-"
+    append_punctuations: str = "\"'.。,，!！?？:：”)]}、"
+    vad_filter: bool = False
+    vad_parameters: Optional[Union[dict, VadOptions]] = None
 
 
 @dataclass
@@ -69,13 +109,8 @@ class FasterWhisperASRSegmentInferencer(WhisperInferencer):
 
     @beartype
     def predict_transcribed_with_whisper_args(
-        self, audio_array: NumpyFloat1D, whisper_args: WhisperArgs
+        self, audio_array: NumpyFloat1D, whisper_args: FasterWhisperArgs
     ):  # -> StartEndTextsNonOverlap:
-        segments, _ = self._model.transcribe(
-            audio_array,
-            beam_size=5,
-            word_timestamps=True,
-            # initial_prompt="Welcome to the software engineering courses channel.",
-        )
+        segments, _ = self._model.transcribe(audio_array, **asdict(whisper_args))
         segments = list(segments)
         return [(seg.start, seg.end, seg.text) for seg in segments]
