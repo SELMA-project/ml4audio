8889bec9a626e5bea1464abceb8814340cdf6e65
Tilo Himmelsbach <dertilo@gmail.com>
2023-08-24 08:46:27 +0200

    time_diff

--- ml4audio/asr_inference/transcript_glueing.py
+++ ml4audio/asr_inference/transcript_glueing.py
@@ -52,45 +52,21 @@ NO_NEW_SUFFIX = _NO_NEW_SUFFIX()
 
 
 @beartype
-def glue_left_right_update_hyp_buffer(
-    new_trans: TimestampedLetters,
-    hyp_buffer: TimestampedLetters,
-    sm: difflib.SequenceMatcher,
-) -> Union[TimestampedLetters, _NO_NEW_SUFFIX]:
-
-    # glueing may fail due to too few overlap
-    new_suffix = just_try(
-        lambda: calc_new_suffix(left=hyp_buffer, right=new_trans, sm=sm),
-        default=NO_NEW_SUFFIX,  # a failed glue does not add anything! In the hope that overlap is big enough so that it can be recovered by next glue!
-        verbose=DEBUG,
-        print_stacktrace=False,
-    )
-    return new_suffix
-
-
-# """
-# two overlapping sequences
-#
-# left:_____----:-
-# right:_______-:----
-#
-# colon is glue-point which is to be found
-#
-# 1. from left cut away what reaches too far into past
-#     left:________-:-
-# 2. from right cut away what reaches too far into future -> just for alignmend-method
-#     right:_______-:--
-#
-# return cut left and full right glued together
-# """
-
-
-@beartype
 def calc_new_suffix(
     left: TimestampedLetters,
     right: TimestampedLetters,
     sm: difflib.SequenceMatcher,
 ) -> Union[TimestampedLetters, _NO_NEW_SUFFIX]:
+    """
+    two overlapping sequences
+
+    left:_____----:-
+    right:_______-:----
+
+    colon is glue-point which is to be found
+
+    return new suffix
+    """
     is_overlapping = left.timestamps[-1] > right.timestamps[0]
     if not is_overlapping:
         if left.letters[-1] != " " and right.letters[0] != " ":
@@ -108,22 +84,23 @@ def calc_new_suffix(
             glue_point_left_cut, glue_point_right = calc_glue_points(
                 left_cut.letters, matches
             )
+            # print(
+            #     f"{left_cut.letters[:glue_point_left_cut]}---{right.letters[glue_point_right:]}"
+            # )
             assert (
                 left_cut.letters[glue_point_left_cut] == right.letters[glue_point_right]
-            ),f"{left_cut.letters[glue_point_left_cut], right.letters[glue_point_right]}"
-            # time_diff = (
-            #     left_cut.timestamps[glue_point_left_cut]
-            #     - right.timestamps[glue_point_right]
-            # )
-            # print(f"{time_diff=}")
+            )
             new_suffix = TimestampedLetters(
-                right.letters[(glue_point_right + 1) :],
-                right.timestamps[(glue_point_right + 1) :],
+                right.letters[(glue_point_right):],
+                right.timestamps[(glue_point_right):],
             )
-            assert (
+            time_diff = (
                 left_cut.timestamps[glue_point_left_cut]
-                < right.timestamps[(glue_point_right + 1)]
+                - right.timestamps[glue_point_right]
             )
+            new_suffix.timestamps[
+                0
+            ] += time_diff  # shift only the very first letter to exactly match the timestamp of the last letter in "left"
         else:
             new_suffix = NO_NEW_SUFFIX
             if DEBUG:
@@ -149,6 +126,13 @@ def cut_left_calc_matches(
     right: TimestampedLetters,
     sm: difflib.SequenceMatcher,
 ) -> tuple[TimestampedLetters, list[difflib.Match]]:
+    """
+    1. from left cut away what reaches too far into past
+        left:________-:-
+    2. from right cut away what reaches too far into future -> just for alignmend-method
+        right:_______-:--
+    3. find matches
+    """
     tol = 0.5  # seconds:  for some reason I wanted half a second "space" to the left
 
     left_cut = left.slice(np.argwhere(left.timestamps > right.timestamps[0] - tol))
@@ -161,21 +145,3 @@ def cut_left_calc_matches(
     sm.set_seqs(left_cut.letters, cut_right_just_to_help_alingment.letters)
     matches = [m for m in sm.get_matching_blocks() if m.size > 0]
     return left_cut, matches
-
-
-@beartype
-def remove_and_append(text: str, suffix_to_be_removed: str, new_suffix: str) -> str:
-    """
-    should only use by client, internally should grow the output via aligned transcripts
-    """
-    raise AssertionError("don't use this anymore!")
-
-    go_k_in_past = len(suffix_to_be_removed)
-    assert (
-        text.endswith(suffix_to_be_removed) or len(suffix_to_be_removed) == 0
-    ), f"{text=} is not ending with {suffix_to_be_removed=}"
-
-    stable_transcript = text[:-go_k_in_past] if go_k_in_past > 0 else text
-
-    text = f"{stable_transcript}{new_suffix}"
-    return text
