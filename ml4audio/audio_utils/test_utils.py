422028a5984cf5608b8a1e68e6ac21dae6bc1ae4
Tilo Himmelsbach <dertilo@gmail.com>
2023-08-26 10:00:19 +0200

    test_utils.py

---
+++ ml4audio/audio_utils/test_utils.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+
+from misc_utils.prefix_suffix import BASE_PATHES, PrefixSuffix
+
+TEST_RESOURCES = "tests/resources"
+BASE_PATHES["test_resources"] = TEST_RESOURCES
+
+
+def get_test_cache_base():
+    cache_base = PrefixSuffix("test_resources", "cache")
+    if os.path.isdir(str(cache_base)):
+        shutil.rmtree(str(cache_base))
+    os.makedirs(str(cache_base))
+    return cache_base
+
+
+def get_test_vocab():
+    return f"""<pad>
+<s>
+</s>
+<unk>
+|
+E
+T
+A
+O
+N
+I
+H
+S
+R
+D
+L
+U
+M
+W
+C
+F
+G
+Y
+P
+B
+V
+K
+'
+X
+J
+Q
+Z""".split(
+        "\n"
+    )
