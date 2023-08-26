c15c3310f7ec9fa2c59dff7e53d99fb51c326dc1
Tilo Himmelsbach <dertilo@gmail.com>
2023-08-26 09:32:57 +0200

    use jiwer

--- ml4audio/text_processing/asr_metrics.py
+++ ml4audio/text_processing/asr_metrics.py
@@ -1,94 +1,51 @@
-from typing import Tuple
-
-import Levenshtein
 import jiwer as jiwer
 from beartype import beartype
 
 from misc_utils.beartypes import NeList
 
-"""
-nemo is using [editdistance](https://github.com/roy-ht/editdistance)
 
-here Levenshtein is used: https://github.com/ztane/python-Levenshtein/
-"""
+@beartype
+def character_error_rates(refs: NeList[str], hyps: NeList[str]) -> dict[str, float]:
+    cho = jiwer.process_characters(refs, hyps)
+    num_chars = sum([len(r) for r in cho.references])
+    return {
+        "cer": cho.cer,
+        "insr": cho.insertions / num_chars,
+        "delr": cho.deletions / num_chars,
+        "subr": cho.substitutions / num_chars,
+        # "hit": cho.hits / num_chars, # who cares about a hit-rate?
+    }
 
 
 @beartype
-def calc_num_word_errors(hyp: str, ref: str) -> Tuple[int, int]:
-    """
-    based on: https://github.com/SeanNaren/deepspeech.pytorch/blob/master/deepspeech_pytorch/decoder.py
-    https://github.com/SeanNaren/deepspeech.pytorch/blob/78f7fb791f42c44c8a46f10e79adad796399892b/deepspeech_pytorch/decoder.py#L42
-    """
-
-    def tokenize(s):
-        return s.split()
-
-    b = set(tokenize(hyp) + tokenize(ref))
-    token2idx = {t: k for k, t in enumerate(b)}
-
-    # map the words to a char array (Levenshtein packages only accepts
-    # strings)
-    w1 = [chr(token2idx[w]) for w in hyp.split()]
-    w2 = [chr(token2idx[w]) for w in ref.split()]
-
-    len_ref = len(ref.split(" "))
-    return Levenshtein.distance("".join(w1), "".join(w2)), len_ref
+def word_error_rates(refs: NeList[str], hyps: NeList[str]) -> dict[str, float]:
+    who = jiwer.process_words(refs, hyps)
+    num_words = sum(len(r) for r in who.references)
+    assert num_words == who.hits + who.deletions + who.substitutions
+    return {
+        "wer": who.wer,
+        "insr": who.insertions / num_words,
+        "delr": who.deletions / num_words,
+        "subr": who.substitutions / num_words,
+        # "hit": who.hits / num_words,
+    }
 
 
-@beartype
-def calc_num_char_errors(hyp: str, ref: str) -> Tuple[int, int]:
-    """
-    based on: https://github.com/SeanNaren/deepspeech.pytorch/blob/78f7fb791f42c44c8a46f10e79adad796399892b/deepspeech_pytorch/decoder.py#L62
-    """
-    hyp, ref, = (
-        hyp.replace(" ", ""),
-        ref.replace(" ", ""),
-    )  # TODO(tilo): why removing spaces?
-
-    len_ref = len(ref.replace(" ", ""))
-    return Levenshtein.distance(hyp, ref), len_ref
+def calc_cer(refs, hyps) -> float:
+    return character_error_rates(refs, hyps)["cer"]
 
 
-@beartype
-def micro_average(errors_lens: NeList[tuple[int, int]]) -> float:
-    num_tokens = sum([l for _, l in errors_lens])
-    errors = sum([s for s, _ in errors_lens])
-    return float(errors) / float(num_tokens) if num_tokens > 0 else -1.0
-
-
-@beartype
-def micro_avg_wer(hyps_targets: NeList[Tuple[str, str]]):
-    errors_lens = [calc_num_word_errors(hyp, ref) for hyp, ref in hyps_targets]
-    return micro_average(errors_lens)
-
-
-@beartype
-def micro_avg_cer(hyps_targets: NeList[Tuple[str, str]]):
-    errors_lens = [calc_num_char_errors(hyp, ref) for hyp, ref in hyps_targets]
-    return micro_average(errors_lens)
-
-
-calc_cer = micro_avg_cer  # old deprecated name
-calc_wer = micro_avg_wer  # old deprecated name
+def calc_wer(refs, hyps) -> float:
+    return word_error_rates(refs, hyps)["wer"]
 
 
 @beartype
 def micro_avg_asr_scores(
-    id_hyp_target: NeList[tuple[str, str, str]]
-) -> dict[str, float]:
-    hyp_targets = [(h, t) for _, h, t in id_hyp_target]
-    # editops=[Levenshtein.editops(h, t) for h,t in hyp_targets]
-    hyps, targets = [list(x) for x in zip(*hyp_targets)]
-    num_chars = sum(len(t) for t in targets)
-    cho = jiwer.process_characters(targets, hyps)
+    refs_hyps: NeList[tuple[str, str]]
+) -> dict[str, dict[str, float]]:
+    refs, hyps = [list(x) for x in zip(*refs_hyps)]
     return {
-        "wer": micro_avg_wer(hyp_targets),
-        "cer": micro_avg_cer(hyp_targets),
-        "ins": cho.insertions / num_chars,
-        "del": cho.deletions / num_chars,
-        "sub": cho.substitutions / num_chars,
-        "hit": cho.hits / num_chars,
+        "word": word_error_rates(refs, hyps),
+        "char": character_error_rates(refs, hyps),
     }
 
-
-calc_asr_scores = micro_avg_asr_scores  # old deprecated name
